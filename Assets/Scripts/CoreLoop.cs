48b6ed0b947b338e015ea9bebe3e2660183259c0
Fabiano Brito <fabianopbrito@gmail.com>
2020-06-29 23:20:48 +0300

    Serialize and display guess number as TMPro

--- Assets/Scripts/CoreLoop.cs
+++ Assets/Scripts/CoreLoop.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CoreLoop : MonoBehaviour
 {
     [SerializeField] int max;
     [SerializeField] int min;
+    [SerializeField] public TextMeshProUGUI stringGuess;
     int guess;
 
     void Start()
@@ -15,13 +17,14 @@ public class CoreLoop : MonoBehaviour
 
     void StartGame()
     {
-        max = max + 1;
         NextGuess();
+        max = max + 1;
     }
 
     void NextGuess()
     {
         guess = (max + min) / 2;
+        stringGuess.text = guess.ToString();
     }
 
     public void OnPressHigher()
