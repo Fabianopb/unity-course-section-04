d05d9a2ce6155045df03c99e9cb04252c3f42932
Fabiano Brito <fabianopbrito@gmail.com>
2020-06-29 23:14:07 +0300

    Add core loop script and hook it up to core scene

---
+++ Assets/Scripts/CoreLoop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreLoop : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = max + 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+}
